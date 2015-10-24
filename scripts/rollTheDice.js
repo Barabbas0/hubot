f7f198d5de8a4894d0b442a7c810a2df1d0191d7
barabbas0 <AndyYankee17@gmail.com>
2015-10-24 16:41:20 +0000

    initial commit

---
+++ scripts/rollTheDice.js
@@ -0,0 +1,32 @@
+/*global
+ module
+ */
+// Description
+//  Detects 'rtd' and returns a random number between 1-6
+//
+// Dependencies:
+//  none
+//
+// Configuration:
+//   none
+//
+// Commands:
+//   <rtd> - <returns a random number between 1-6>
+//
+// Notes:
+//   none
+//
+// Author:
+//   Andrew Ahern
+/**
+ *
+ * @param robot
+ */
+module.exports = function (robot) {
+  robot.hear(/rtd/i, function (response) {
+      var min = 1;
+      var max = 6;
+      var rand = Math.floor(Math.random() * (max - min + 1)) + min;
+    response.send('You rolled a ' + rand);
+  });
+};
